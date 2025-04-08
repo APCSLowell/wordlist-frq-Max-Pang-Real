acd775b9d20cb5a604188e866c14af470b83493b
Max-Pang-Real <mapang@s.sfusd.edu>
2025-04-08 11:54:00 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -8,15 +8,21 @@ public class WordList
   public void add(String word) { myList.add(word); }
   public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
-    /* to be implemented in part (a) */
-    
+  public int numWordsOfLength(int len){
+    int count = 0;
+    for(int i = 0; i < myList.size(); i++){
+      if(mtList.get(i).length().equals(len)){
+        count++;
+      }
+    }
+    return count;
   }
 
-  public void removeWordsOfLength(int len)
-  {
-    /* to be implemented in part (b) */
-
+  public void removeWordsOfLength(int len){
+   for(int i = myList.size()-1; i >= 0; i--){
+     if(myList.get(i).length().equals(len)){
+       myList.remove(i);
+     }
+   }
   }
 }
